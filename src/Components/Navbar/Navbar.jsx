4f443dbf1bbd9ff42a286b1d25cb3d24531638f8
GK Malinda <164718081+krishaCode@users.noreply.github.com>
2025-09-21 23:56:33 +0530

    Add React-Bootstrap navbar and asset files
    Replaced custom Navbar with a React-Bootstrap implementation, including scroll and active link handling. Added font and image assets to the project. Updated dependencies to include bootstrap and react-bootstrap.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
-import './Navbar.css'
-
+import React, { useEffect } from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import { Navbar as BootstrapNavbar } from 'react-bootstrap';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { useState } from 'react';
 const Navbar = () => {
+  const[activeLink, setActiveLink] = useState('home');
+  const[scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if(window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [])
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
+
   return (
-    <div className='navbar'>Navbar</div>
+     <BootstrapNavbar expand="lg" className={scrolled ? "scrolled" : ""} >
+      <Container>
+        <BootstrapNavbar.Brand href="#home">React-Bootstrap</BootstrapNavbar.Brand>
+        <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" >
+          <span className="navbar-toggler-icon"></span>
+        </BootstrapNavbar.Toggle>
+        <BootstrapNavbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
+            <Nav.Link href="#skill" className={activeLink === 'skill' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skill')}>Skill</Nav.Link>
+            <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')} >Projects</Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-icons">
+              <a href="#"><img src='https://via.placeholder.com/30' alt='Social Icon 1'/></a>
+              <a href="#"><img src='https://via.placeholder.com/30' alt='Social Icon 2'/></a>
+              <a href="#"><img src='https://via.placeholder.com/30' alt='Social Icon 3'/></a>
+            </div>
+            <button className="vvd" onClick={() => console.log("Connect button clicked")}><span>Let’s Connect</span></button>
+          </span>
+        </BootstrapNavbar.Collapse>
+      </Container>
+    </BootstrapNavbar>
   )
 }
 
